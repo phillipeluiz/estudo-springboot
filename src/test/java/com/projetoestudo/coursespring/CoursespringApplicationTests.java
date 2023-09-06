8aba904a20c905961b36254b85016d90791ddd2e
Phillipe <phillipeluiz@outlook.com>
2023-09-06 09:08:29 -0300

    initial commit

---
+++ src/test/java/com/projetoestudo/coursespring/CoursespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoestudo.coursespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
