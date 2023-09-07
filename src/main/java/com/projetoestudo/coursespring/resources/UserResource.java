879dea8d5ff2a9aea340f3d18d7bcce0a518c385
Phillipe <phillipeluiz@outlook.com>
2023-09-07 11:32:59 -0300

    User entity and resource

---
+++ src/main/java/com/projetoestudo/coursespring/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.projetoestudo.coursespring.resources;
+
+
+import com.projetoestudo.coursespring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Phillipe", "phillipe@outlook.com", "34999999", "12345678");
+
+
+        return ResponseEntity.ok().body(u);
+    }
+}
