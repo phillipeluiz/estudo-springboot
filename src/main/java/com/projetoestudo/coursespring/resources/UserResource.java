9605bed329e1cba2871045f6a3b829ee0cd58220
Phillipe <phillipeluiz@outlook.com>
2023-11-08 19:05:15 -0300

    User insert

--- src/main/java/com/projetoestudo/coursespring/resources/UserResource.java
+++ src/main/java/com/projetoestudo/coursespring/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.projetoestudo.coursespring.entities.User;
 import com.projetoestudo.coursespring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +31,13 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+
     }
 }
--- src/main/java/com/projetoestudo/coursespring/service/UserService.java
+++ src/main/java/com/projetoestudo/coursespring/service/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
         return user.get();
     }
 
+    public User insert(User obj){
+        return userRepository.save(obj);
+    }
+
 }
